7159d5cfc756df65ebf841b8c8b1ab25a869aeda
Toba <marxdm02@gmail.com>
2021-03-27 13:11:44 +0000

    管理者機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action user_admin, only: [:index]
+  
   def new
     @user = User.new
   end
@@ -16,5 +18,14 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def user_admin
+    @users = User.all
+    if current_user.admin == false
+      redirect_to root_path
+    else
+      render action: "index"
+    end
   end
 end
